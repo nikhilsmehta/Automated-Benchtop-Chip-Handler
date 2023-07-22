bbcdc5e45a5648a79a761b30d8003f8fe1faa7bd
Nikhil Mehta <67570213+nikhilsmehta@users.noreply.github.com>
2023-07-22 14:27:39 -0700

    Update gcode test.py

--- Python GCODE/gcode test.py
+++ Python GCODE/gcode test.py
@@ -1,5 +1,5 @@
-from mecode import G
+import mecode
+g = mecode.G(direct_write=True, direct_write_mode="serial", printer_port="/dev/tty.usbmodem1411", baudrate=115200)
 
-g = G()
-
-g.move(10, 10)
+g.move(x=10, y=10, z=0, F=500)
+g.current_position()
